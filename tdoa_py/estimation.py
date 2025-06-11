bcca7544537e9f32168f009a5994456fc9027171
Abel Lamas <abellamasm@gmail.com>
2025-06-11 16:33:58 -0300

    Funciona bien hasta 90°

--- tdoa_py/estimation.py
+++ tdoa_py/estimation.py
@@ -1,25 +1,51 @@
 import numpy as np
 from scipy.signal import correlate, correlation_lags
 
-def estimate_tdoa(sig1, sig2, fs):
-    corr = correlate(sig1, sig2, mode='full')  # mic1 sobre mic2
-    lags = correlation_lags(len(sig1), len(sig2), mode='full')
+def estimate_tdoa(sig_ref, sig, fs):
+    corr = correlate(sig, sig_ref, mode='full')
+    lags = correlation_lags(len(sig), len(sig_ref), mode='full')
     lag = lags[np.argmax(corr)]
     return lag / fs
 
-def estimate_doa_from_array_axis(signals, fs, d, c=343.0):
-    ref_signal = signals[0]
-    tdoas = []
+def estimate_doa(signals, d, fs, c=343.0):
+    ref_idx = 0
+    ref_signal = signals[ref_idx]
+    n_mics = len(signals)
 
-    for i in range(1, len(signals)):
+    tdoas = []
+    for i in range(n_mics):
+        if i == ref_idx:
+            continue
         tdoa = estimate_tdoa(ref_signal, signals[i], fs)
         tdoas.append(tdoa)
 
-    avg_tdoa = np.mean(tdoas)
-    argument = avg_tdoa * c / d
-    argument = np.clip(argument, -1.0, 1.0)
+    distances = [abs(i - ref_idx) * d for i in range(n_mics) if i != ref_idx]
 
-    phi_rad = np.arccos(argument)
-    phi_deg = np.degrees(phi_rad)
+    angles = []
+    weighted_sum = 0
+    idx = 0
+    for i in range(n_mics):
+        if i == ref_idx:
+            continue
+        dist = distances[idx]
+        tdoa = tdoas[idx]
+        idx += 1
 
-    return phi_deg, tdoas
+        arg = tdoa * c / dist
+        arg = np.clip(arg, -1.0, 1.0)
+        phi_rad = np.arccos(arg)
+        phi_deg = np.degrees(phi_rad)
+
+        angles.append(phi_deg)
+
+        weighted_sum += np.sign(i - ref_idx) * tdoa
+
+    mean_angle = np.mean(angles)
+
+    # INVERTIMOS la condición para corregir el problema
+    if weighted_sum > 0:
+        doa = mean_angle  # Fuente a la derecha → 0° a 90°
+    else:
+        doa = 180 - mean_angle  # Fuente a la izquierda → 90° a 180°
+
+    return doa, tdoas
