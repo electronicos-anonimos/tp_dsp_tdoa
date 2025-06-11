c39241aff253b1c09ca523ec5702de7e3c233f6b
Abel Lamas <abellamasm@gmail.com>
2025-06-11 17:03:58 -0300

    Funciona PHAT

--- tdoa_py/estimation.py
+++ tdoa_py/estimation.py
@@ -1,13 +1,30 @@
 import numpy as np
 from scipy.signal import correlate, correlation_lags
+from scipy.fft import fft, ifft
 
-def estimate_tdoa(sig_ref, sig, fs):
-    corr = correlate(sig_ref, sig, mode='full')
-    lags = correlation_lags(len(sig), len(sig_ref), mode='full')
-    lag = lags[np.argmax(corr)]
-    return lag / fs
+def estimate_tdoa(sig_ref, sig, fs, method='classic'):
+    if method == 'classic':
+        corr = correlate(sig_ref, sig, mode='full')
+        lags = correlation_lags(len(sig), len(sig_ref), mode='full')
+        lag = lags[np.argmax(corr)]
+        return lag / fs
 
-def estimate_doa(signals, d, fs, c=343.0):
+    elif method == 'phat':
+        n = len(sig_ref) + len(sig) - 1
+        X1 = fft(sig_ref, n=n)
+        X2 = fft(sig, n=n)
+        R = X1 * np.conj(X2)
+        R /= np.abs(R) + 1e-15  # Evitar división por cero
+        corr = np.real(ifft(R))
+        lags = np.arange(-n // 2, n // 2 + 1)
+        corr = np.roll(corr, n // 2)
+        lag = lags[np.argmax(corr)]
+        return lag / fs
+
+    else:
+        raise ValueError("Método no reconocido. Usar 'classic' o 'phat'.")
+
+def estimate_doa(signals, d, fs, c=343.0, method='classic'):
     ref_idx = 0
     ref_signal = signals[ref_idx]
     n_mics = len(signals)
@@ -16,7 +33,7 @@ def estimate_doa(signals, d, fs, c=343.0):
     for i in range(n_mics):
         if i == ref_idx:
             continue
-        tdoa = estimate_tdoa(ref_signal, signals[i], fs)
+        tdoa = estimate_tdoa(ref_signal, signals[i], fs, method)
         tdoas.append(tdoa)
 
     distances = [abs(i - ref_idx) * d for i in range(n_mics) if i != ref_idx]
@@ -37,10 +54,7 @@ def estimate_doa(signals, d, fs, c=343.0):
         phi_deg = np.degrees(phi_rad)
 
         angles.append(phi_deg)
-
         weighted_sum += np.sign(i - ref_idx) * tdoa
 
     doa = np.mean(angles)
-
-
     return doa, tdoas
